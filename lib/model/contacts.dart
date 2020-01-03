ab89a9053dd33948ff50cb8fa90d9fd77f0be5ea
windrunner <1626023124@qq.com>
2020-01-04 02:16:15 +0800

    提交一下

--- lib/model/contacts.dart
+++ lib/model/contacts.dart
@@ -18,11 +18,13 @@ class Contact extends BaseModel {
 
 @CopyWith()
 class ContactsPageData extends BaseModel {
-  const ContactsPageData({this.contacts});
+  // ignore: prefer_const_constructors_in_immutables, const list不能sort
+  ContactsPageData({this.contacts});
 
   final List<Contact> contacts;
 
   static ContactsPageData mock() {
+    // ignore: prefer_const_literals_to_create_immutables, const list不能sort
     return ContactsPageData(contacts: <Contact>[
       const Contact(
         avatar: 'https://randomuser.me/api/portraits/men/50.jpg',
